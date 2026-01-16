212e24d62f55cfbeeee62d67145a176b9099879f
prplhd <dimas.kovalenko2000@mail.ru>
2026-01-16 17:30:34 +0700

    feat(db): add initial Flyway migrations for schema and seed data

---
+++ src/main/resources/db/migration/V2__init_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO players(name) VALUES ('Nadya');
+INSERT INTO players(name) VALUES ('Dmitry');
+INSERT INTO players(name) VALUES ('Fedor');
+
+INSERT INTO matches(player1, player2, winner) VALUES (1, 2, 2);
+
+INSERT INTO matches(player1, player2, winner) VALUES (3, 1, 1);
+
+INSERT INTO matches(player1, player2, winner) VALUES (2, 3, 3);
