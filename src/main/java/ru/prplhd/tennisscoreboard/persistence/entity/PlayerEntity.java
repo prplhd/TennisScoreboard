ea07a8cdfb729947dd8edb028709d03aafe6b525
prplhd <dimas.kovalenko2000@mail.ru>
2026-01-16 17:27:53 +0700

    feat(db): map PlayerEntity and FinishedMatchEntity with JPA annotations

--- src/main/java/ru/prplhd/tennisscoreboard/persistence/entity/PlayerEntity.java
+++ src/main/java/ru/prplhd/tennisscoreboard/persistence/entity/PlayerEntity.java
@@ -1,4 +1,21 @@
 package ru.prplhd.tennisscoreboard.persistence.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "players")
 public class PlayerEntity {
+    @Id
+    private Integer id;
+    private String name;
 }
