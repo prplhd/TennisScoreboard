792e09cf56929e3313a9480f60fa82b5bda9447a
prplhd <dimas.kovalenko2000@mail.ru>
2026-03-28 18:30:26 +0700

    refactor(core): use RequiredArgsConstructor instead of AllArgsConstructor in Player

--- src/main/java/ru/prplhd/tennisscoreboard/domain/Player.java
+++ src/main/java/ru/prplhd/tennisscoreboard/domain/Player.java
@@ -1,10 +1,10 @@
 package ru.prplhd.tennisscoreboard.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class Player {
 
     private final String name;
