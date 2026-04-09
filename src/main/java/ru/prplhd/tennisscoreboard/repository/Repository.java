621d4a9dfa13722ca89e24307b72c9057f08a5b7
prplhd <dimas.kovalenko2000@mail.ru>
2026-04-09 16:50:01 +0700

    refactor(db): remove findAll() from repository contract

--- src/main/java/ru/prplhd/tennisscoreboard/repository/Repository.java
+++ src/main/java/ru/prplhd/tennisscoreboard/repository/Repository.java
@@ -1,14 +1,11 @@
 package ru.prplhd.tennisscoreboard.repository;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Optional;
 
 public interface Repository<ID extends Serializable, E> {
 
     Optional<E> findById(ID id);
-
-    List<E> findAll();
 
     E save(E entity);
 
